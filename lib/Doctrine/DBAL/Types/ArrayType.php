9e51d5ee27ae837a54f28eed25839293e0e6fbd3
Andy Bell <a.bell@texthelp.com>
2020-04-16 15:18:31 +0100

    added a base64_decode before unserializing

--- lib/Doctrine/DBAL/Types/ArrayType.php
+++ lib/Doctrine/DBAL/Types/ArrayType.php
@@ -55,7 +55,12 @@ class ArrayType extends Type
         }
 
         $value = (is_resource($value)) ? stream_get_contents($value) : $value;
-        $val = unserialize($value);
+        if (!base64_decode($value, true)) {
+            $val = unserialize($value);
+        } else {
+            $val = unserialize(base64_decode($value));
+        }
+
         if ($val === false && $value != 'b:0;') {
             throw ConversionException::conversionFailed($value, $this->getName());
         }
